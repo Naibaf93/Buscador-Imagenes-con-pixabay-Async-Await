52d581150b6cfee58e8ff835b92ab7c793f1a36c
Fabián Huertas <miloh9318@hotmail.com>
2025-10-04 20:30:50 -0500

    moviendo el proyecto con async await

--- js/app.js
+++ js/app.js
@@ -47,19 +47,31 @@ function mostrarAlerta(mensaje) {
     }
 }
 
-function buscarImagenes() {
+async function buscarImagenes() {
 
     const termino = document.querySelector('#termino').value;
 
     const key = '47081279-bec481177116f194a9c59e1bf';
     const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
 
+    /*
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(resultado => {
             totalPaginas = calcularPaginas(resultado.totalHits);
             mostrarImagenes(resultado.hits);
-        })   
+        });
+    */    
+        
+    try {
+        const respuesta =  await fetch(url);
+        const resultado = await respuesta.json();
+        totalPaginas = calcularPaginas(resultado.totalHits);
+        mostrarImagenes(resultado.hits);
+    } catch (error) {
+        console.log(error);
+        
+    }    
 }
 
 // Generador que va a registrar la cantidad de elementos de acuerdo a las paginas
